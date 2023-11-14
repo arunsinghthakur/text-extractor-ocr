a06a993ec5370df50cd17d8b1294d7b8820b5343
ARUN SINGH <arun_gkv@yahoo.co.in>
2023-11-14 14:27:39 +0530

    Add files via upload

---
+++ tesseract-ocr/src/index.js
@@ -0,0 +1,24 @@
+﻿const Tesseract = require('tesseract.js');
+
+window.addEventListener("message", (event) => {
+  const url = event.data;
+
+  Tesseract.recognize(
+    url,
+    "eng",
+    { logger: m => console.log(m) }
+  ).then(({ data: { text } }) => {
+    document.getElementById("text").innerText = text;
+  })
+
+});
+
+
+const copyButton = document.getElementById("copyText");
+copyButton.onclick = function () {
+  navigator.clipboard.writeText(document.getElementById("text").innerText).then(() => {
+    console.log('Text copied to clipboard!');
+  }).catch(err => {
+    console.log('Failed to copy text: ', err);
+  });
+};
